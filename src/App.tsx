2ef3c97f492ab7e57c17b280864a4f14c1f9e39c
Eduardo Fernandes <ducabruno09@hotmail.com>
2022-10-23 21:11:03 -0400

    chore: del stories and add tailwind theme

--- src/App.tsx
+++ src/App.tsx
@@ -5,9 +5,9 @@ export function App() {
 
   return (
     <>
-      <h1 className="font-bold text-5xl text-violet-500">Hello World</h1>
+      <h1 className="font-bold text-2xl text-gray-900">Hello World</h1>
       <button
-        className="px-4 py-2 border rounded border-red-500"
+        className="px-4 py-2 border rounded border-gray-400"
         onClick={() => {
           setCount(count + 1);
         }}
