887a31ff6598687d80452ac128b13877221aac63
Eduardo Fernandes <ducabruno09@hotmail.com>
2022-10-23 21:39:31 -0400

    feat: add Text component with Radix slot, clsx and story with typing

---
+++ src/components/Text.stories.tsx
@@ -0,0 +1,32 @@
+import { Meta, StoryObj } from "@storybook/react";
+import { Text, TextProps } from "./Text";
+
+export default {
+  title: "Components/Text",
+  component: Text,
+  args: {
+    children: "This is a Text component.",
+  },
+} as Meta<TextProps>;
+
+export const Default: StoryObj<TextProps> = {};
+
+export const Small: StoryObj<TextProps> = {
+  args: {
+    size: "sm",
+  },
+};
+
+export const Large: StoryObj<TextProps> = {
+  args: {
+    size: "lg",
+  },
+};
+
+export const CustomElement: StoryObj<TextProps> = {
+  args: {
+    size: "lg",
+    asChild: true,
+    children: <p>This is a Text component with a custom element.</p>,
+  },
+};
