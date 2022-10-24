a64fa466172363fc6e5012e7548e3f1dfb293e37
Eduardo Fernandes <ducabruno09@hotmail.com>
2022-10-23 20:24:02 -0400

    chore: add tailwindcss

--- src/App.tsx
+++ src/App.tsx
@@ -5,14 +5,16 @@ export function App() {
 
   return (
     <>
-      <h1>Hello World</h1>
+      <h1 className="font-bold text-5xl text-violet-500">Hello World</h1>
       <button
+        className="px-4 py-2 border rounded border-red-500"
         onClick={() => {
           setCount(count + 1);
         }}
       >
-        {count}
+        increment
       </button>
+      <p>{count}</p>
     </>
   );
 }
